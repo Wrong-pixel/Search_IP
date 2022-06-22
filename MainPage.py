1cc372e276161cb09c677896e7fb004511a7866d
bigbigban1 <jackie_w3@yeah.net>
2022-06-22 16:10:47 +0800

    简化代码，优化导入

--- MainPage.py
+++ MainPage.py
@@ -1,6 +1,58 @@
 import tkinter as tk
 from tkinter import ttk
-from ip_Handle import ip_handle
+import sv_ttk
+import re
+import ipaddress
+import json
+
+
+class ip_handle:
+    def __init__(self, json_file, result_IP_Data={}):
+        # 这里可以放在json_Load_IP方法中定义
+        self.result_IP_Data = result_IP_Data
+        # 实例化时需要传入json文件名，实际上也可以放在json_Load_IP方法中去定义
+        self.json_file = json_file
+        # 强制调用json_Load_IP方法，将json文件中的数据处理为ipaddress.ip_network对象
+        self.json_Load_IP()
+
+    def json_Load_IP(self):
+        # 读文件
+        with open("./database.json", encoding='utf-8') as f:
+            ip_data = json.loads(f.read())['children']
+        # 将ip段转化为ipaddress.ip_network对象
+        for item in ip_data:
+            temp_data = []
+            try:
+                for ips in item["networks"]:
+                    temp_data.append(ipaddress.ip_network(ips, False))
+                self.result_IP_Data.update({item["name"]: temp_data})
+            except ValueError:
+                pass
+        return self.result_IP_Data
+
+    # 输入IP合法性娇艳
+    def check_ip(self, ipAddr):
+        compile_ip = re.compile(
+            '^(1\d{2}|2[0-4]\d|25[0-5]|[1-9]\d|[1-9])\.(1\d{2}|2[0-4]\d|25[0-5]|[1-9]\d|\d)\.(1\d{2}|2[0-4]\d|25[0-5]|[1-9]\d|\d)\.(1\d{2}|2[0-4]\d|25[0-5]|[1-9]\d|\d)$'
+        )
+        if compile_ip.match(ipAddr):
+            return True
+        else:
+            return False
+
+    # 查找IP，不要问为啥不用二分法、问就是不会
+    def search_ip(self, ipaddr):
+        if self.check_ip(ipaddr):
+            try:
+                for key in self.result_IP_Data:
+                    for item in self.result_IP_Data[key]:
+                        if ipaddress.ip_address(ipaddr) in item:
+                            return True, key
+                return False, None
+            except BaseException:
+                return False, None
+        else:
+            return False, None
 
 
 class App(ttk.Frame):
@@ -70,8 +122,7 @@ if __name__ == '__main__':
     ip_module = ip_handle("./database.json")
 
     # 设置主题
-    root.tk.call("source", "sun-valley.tcl")
-    root.tk.call("set_theme", "light")
+    sv_ttk.set_theme("light")
 
     # 设置窗体
     root.geometry('285x75+{}+{}'.format(
